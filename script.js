52114d13b2df97c252ec21bc94d4f18cfdf1339a
Fryderyk Kruczek <fryderykkruczek@gmail.com>
2020-01-04 01:23:41 +0100

    with fetch api

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-let url = 'http://www.cioff.org/events-festival.cfm/en/1042/Croatia-International_Festival_of_Traditional_Music_instruments';
-let fileUrl = 'festiwale.txt';
-function httpGet(theUrl) {
-    return new Promise((resolve, reject) => {
-        if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        }
-        else {// code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                resolve(xmlhttp.response);
-            }
-        }
-        xmlhttp.open("GET", theUrl, false);
-        xmlhttp.send();
-    });
-}
-
 async function readTextFile(file) {
     return new Promise((resolve, reject) => {
         var rawFile = new XMLHttpRequest();
@@ -34,8 +14,11 @@ async function readTextFile(file) {
     });
 }
 
-let urls = [];
-let emails = [];
+async function httpGet(url) {
+    const response = await fetch(url);
+    const text = await response.text();
+    return getEmailFromSite(text);
+}
 
 let extractUrls = /http:[^"]+/gm;
 let extractFunction = /urlformsg\(\s*([^)]+?)\s*\)/m;
@@ -47,34 +30,12 @@ function getEmailFromSite(site) {
     return params[0] + '@' + params[1];
 }
 
-
-// TO DZIAŁA
-/*
 async function getEmails() {
-    let email, emails = [];
-    let file = await readTextFile(fileUrl);
+    let file = await readTextFile('festiwale.txt');
     let urls = file.match(extractUrls);
-    console.log(urls.length);
-    for (let i = 0; i < url.length; i++) {
-        let site = await httpGet(urls[i]);
-        email = getEmailFromSite(site);
-        emails.push(email);
-        console.log(email);
-    }
+    let promises = urls.map(url => httpGet(url));
+    let emails = await Promise.all(promises);
     document.getElementById('content').innerText = emails;
 }
-*/
-
-//TO NIE DIZAŁA
-async function getEmails() {
-
-    let file = await readTextFile(fileUrl);
-    let urls = file.match(extractUrls);
-
-    let promises = urls.map(url => httpGet(url))
-    let emails = await Promise.all(promises); //promises to kody źródłowe stron chyba, jeszcze funkcja getEmailFromSite()???
-
-    document.getElementById('content').innerText = emails;
-}
-
 getEmails();
+
